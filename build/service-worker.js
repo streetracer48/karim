07d996163bd660764df665e31160c7060dff7028
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-09-28 14:33:16 +0600

    data added

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/miraz/precache-manifest.2ea469b1dc25fdce63c7e12ab5b591fb.js"
+  "/miraz/precache-manifest.939874290d0d1088731556347a4d009b.js"
 );
 
 workbox.clientsClaim();
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { CenteredHeader } from './App.js'
+import { CenteredHeader } from "./App.js";
 
 class Home extends Component {
   render() {
     return (
       <div>
-      <CenteredHeader>Hi! I'm Miraz . software developer, in other words, a creative technologist based in Dhaka Bangladesh.</CenteredHeader>
-      <p>
-         I currently build software for the Spring Rain IT LTD(http://www.springrainit.com/) at the Dhaka Bangladesh.
-         I  loved to build stuff through code ,  I'm passionate about problem sloving and technology.
-      </p>
-      <p>
-      I am an Engineer meets Designer. With a focus on large scale, high impact, Open Source projects.  I've spent most of my life focused on explored JS for the last few years.  I work almost exclusively in the open, and aim to make almost all of my contributions industry-wide impact.
-      </p>
-      <p>
-        ​Outside of code, I like to create in other ways. From
-         photography to travel, long driving , I'm always trying new ways of expressing myself. I also like to meditate, talk to strangers, and run lifestyle experiments.
-      </p>
-     
-    </div>
+        <CenteredHeader>
+          Hi! I'm Miraz . software developer, in other words, a creative
+          technologist based in Kokkola Finland.
+        </CenteredHeader>
+        <p>
+          before moving Kokkola I've build software for the Spring Rain IT
+          LTD(http://www.springrainit.com/) at the Dhaka Bangladesh.(where i
+          grow) I loved to build stuff through code , I'm passionate about
+          problem sloving and technology.
+        </p>
+        <p>
+          I am an Engineer meets Designer. With a focus on large scale, high
+          impact, Open Source projects. I've spent most of my life focused on
+          explored JS for the last few years. I work almost exclusively in the
+          open, and aim to make almost all of my contributions industry-wide
+          impact.
+        </p>
+        <p>
+          ​Outside of code, I like to create in other ways. From photography to
+          travel, long driving , I'm always trying new ways of expressing
+          myself. I also like to meditate, talk to strangers, and run lifestyle
+          experiments.
+        </p>
+      </div>
     );
   }
 }
